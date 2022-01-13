6d57833f7dc726cf1a6fb2bb1b1331f6f27ea921
Mark Goetz <breakawayblue@gmail.com>
2022-01-12 22:28:56 -0600

    Add stubs for typography and API methods

--- src/lib/api/card.ts
+++ src/lib/api/card.ts
@@ -8,10 +8,24 @@ export const getCommander = async (colors: string[]): Promise<CardResponse> => {
     return response.data;
 };
 
-export const getCards = async (colors: string[], query?: string): Promise<CardResponse> => {
+export const getSpells = async (colors: string[], query?: string): Promise<CardResponse> => {
     const cardQuery = `id<=${colors.join('+')}+f:brawl`;
 
     const response = await axios.get(`https://api.scryfall.com/cards/search?${cardQuery}`);
     return response.data;
 };
 
+export const getBasicLands = async (color: string): Promise<CardResponse> => {
+    const cardQuery = `id=${color}+f:brawl+t:land+t:basic+game=arena&unique=art`;
+
+    const response = await axios.get(`https://api.scryfall.com/cards/search?${cardQuery}`);
+    return response.data;
+};
+
+export const getNonBasicLands = async (colors: string[]): Promise<CardResponse> => {
+    /* https://scryfall.com/search?as=grid&order=name&page=2&q=swamp+t%3Aland+t%3Abasic+game%3Aarena&unique=art */
+    const cardQuery = `id<=${colors.join('+')}+f:brawl`;
+
+    const response = await axios.get(`https://api.scryfall.com/cards/search?${cardQuery}`);
+    return response.data;
+};
