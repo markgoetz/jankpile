fc43edeb57d9bd0f2da0c1f9827e12a065691a69
Mark Goetz <breakawayblue@gmail.com>
2022-01-16 21:23:22 -0600

    Odds and ends fixes

--- src/components/IdentityPanel.tsx
+++ src/components/IdentityPanel.tsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import Color from '../definitions/Color';
 import { setFormat, selectFormat, toggleColor, selectColors } from '../redux-modules/identity';
 import { fetchCommanders  } from '../redux-modules/commander';
+import PanelHeading from './PanelHeading';
 
 const IdentityPanel: React.FC = () => {
     const colors = useSelector(selectColors);
@@ -17,38 +18,40 @@ const IdentityPanel: React.FC = () => {
     );
 
     return (
-        <div>
+        <PanelHeading>
             <div>
-                <h2>Format</h2>
-                <label>
-                    <input type="radio" name="format" value="brawl" checked={format === 'brawl'} onChange={() => dispatch(setFormat('brawl'))} />Brawl
-                </label>
-                <label>
-                    <input type="radio" name="format" value="historic" checked={format === 'historic'} onChange={() => dispatch(setFormat('historic'))} />Historic Brawl
-                </label>
-            </div>
-            <div>
-                <h2>Colors</h2>
-                <label>
-                    <input type="checkbox" name="color" value="W" checked={colors.includes(Color.WHITE)} onChange={() => dispatch(toggleColor(Color.WHITE))} /> White
-                </label>
-                <label>
-                    <input type="checkbox" name="color" value="U" checked={colors.includes(Color.BLUE)} onChange={() => dispatch(toggleColor(Color.BLUE))} /> Blue
-                </label>
-                <label>
-                    <input type="checkbox" name="color" value="B" checked={colors.includes(Color.BLACK)} onChange={() => dispatch(toggleColor(Color.BLACK))} /> Black
-                </label>
-                <label>
-                    <input type="checkbox" name="color" value="R" checked={colors.includes(Color.RED)} onChange={() => dispatch(toggleColor(Color.RED))} /> Red
-                </label>
-                <label>
-                    <input type="checkbox" name="color" value="G" checked={colors.includes(Color.GREEN)} onChange={() => dispatch(toggleColor(Color.GREEN))} /> Green
-                </label>
+                <div>
+                    <h2>Format</h2>
+                    <label>
+                        <input type="radio" name="format" value="brawl" checked={format === 'brawl'} onChange={() => dispatch(setFormat('brawl'))} />Brawl
+                    </label>
+                    <label>
+                        <input type="radio" name="format" value="historic" checked={format === 'historic'} onChange={() => dispatch(setFormat('historic'))} />Historic Brawl
+                    </label>
+                </div>
+                <div>
+                    <h2>Colors</h2>
+                    <label>
+                        <input type="checkbox" name="color" value={Color.WHITE} checked={colors.includes(Color.WHITE)} onChange={() => dispatch(toggleColor(Color.WHITE))} /> White
+                    </label>
+                    <label>
+                        <input type="checkbox" name="color" value={Color.BLUE} checked={colors.includes(Color.BLUE)} onChange={() => dispatch(toggleColor(Color.BLUE))} /> Blue
+                    </label>
+                    <label>
+                        <input type="checkbox" name="color" value={Color.BLACK} checked={colors.includes(Color.BLACK)} onChange={() => dispatch(toggleColor(Color.BLACK))} /> Black
+                    </label>
+                    <label>
+                        <input type="checkbox" name="color" value={Color.RED} checked={colors.includes(Color.RED)} onChange={() => dispatch(toggleColor(Color.RED))} /> Red
+                    </label>
+                    <label>
+                        <input type="checkbox" name="color" value={Color.GREEN} checked={colors.includes(Color.GREEN)} onChange={() => dispatch(toggleColor(Color.GREEN))} /> Green
+                    </label>
+                </div>
             </div>
             <div>
                 <button type="button" onClick={onSearchClick}>Find Commanders</button>
             </div>
-        </div>
+        </PanelHeading>
     );
 };
 
--- src/lib/api/card.ts
+++ src/lib/api/card.ts
@@ -3,15 +3,16 @@ import Color from '../../definitions/Color';
 import CardResponse from '../../definitions/dto/CardResponse';
 import Format from '../../definitions/Format';
 
-const getApiFormat = (format: Format) => (format === 'historic') ? 'historicbrawl' : 'brawl';
+const ENDPOINT = 'https://api.scryfall.com/cards/search?q='
 
+const getApiFormat = (format: Format) => (format === 'historic') ? 'historicbrawl' : 'brawl';
 
 export const getCommander = async (format: Format, colors: Color[]): Promise<CardResponse> => {
     const apiFormat = getApiFormat(format);
 
     const cardQuery = `id=${colors.join('')}+f:${apiFormat}+(is:commander OR t:planeswalker)+game:arena`;
 
-    const response = await axios.get(`https://api.scryfall.com/cards/search?q=${cardQuery}`);
+    const response = await axios.get(`${ENDPOINT}${cardQuery}`);
     return response.data;
 };
 
@@ -19,21 +20,20 @@ export const getSpells = async (format: Format, colors: Color[], query?: string)
     const apiFormat = getApiFormat(format);
     const cardQuery = `id<=${colors.join('+')}+f:${apiFormat}+game:arena`;
 
-    const response = await axios.get(`https://api.scryfall.com/cards/search?${cardQuery}`);
+    const response = await axios.get(`${ENDPOINT}${cardQuery}`);
     return response.data;
 };
 
 export const getBasicLands = async (color: Color): Promise<CardResponse> => {
     const cardQuery = `id=${color}+f:brawl+t:land+t:basic+game:arena&unique=art`;
 
-    const response = await axios.get(`https://api.scryfall.com/cards/search?${cardQuery}`);
+    const response = await axios.get(`${ENDPOINT}${cardQuery}`);
     return response.data;
 };
 
 export const getNonBasicLands = async (colors: Color[]): Promise<CardResponse> => {
-    /* https://scryfall.com/search?as=grid&order=name&page=2&q=swamp+t%3Aland+t%3Abasic+game%3Aarena&unique=art */
     const cardQuery = `id<=${colors.join('+')}+f:brawl+t:land+game:arena`;
 
-    const response = await axios.get(`https://api.scryfall.com/cards/search?${cardQuery}`);
+    const response = await axios.get(`${ENDPOINT}${cardQuery}`);
     return response.data;
 };
