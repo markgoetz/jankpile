93ff8869d3c790145fed4233b310cb6bc3116237
Mark Goetz <breakawayblue@gmail.com>
2022-01-20 22:11:44 -0600

    Retrieve total deck size

--- src/redux-modules/store.ts
+++ src/redux-modules/store.ts
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { CARD_COUNT_BY_FORMAT } from '../lib/consts';
 import commanderSlice from './commander';
 import identitySlice from './identity';
 import landSlice from './lands';
@@ -19,3 +20,19 @@ export default store;
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
+
+export const selectTotalCardCount = (state: RootState) => {
+    const commanderCount = state.commander != null ? 1 : 0;
+    const spellCount = state.spells.spells.length;
+    const nonBasicLandsCount = state.lands.options.length;
+    const basicLandsCount = Object.values(state.lands.basics).reduce((prev, current) => prev + current, 0);
+
+    return commanderCount + spellCount + nonBasicLandsCount + basicLandsCount;
+};
+
+export const selectIsDeckComplete = (state: RootState) => {
+    const totalCount = selectTotalCardCount(state);
+    const format = state.identity.format;
+
+    return totalCount === CARD_COUNT_BY_FORMAT[format];
+};
