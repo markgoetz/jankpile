d92298a6ec0462bd39877d64316e47eb9d6c7a35
Mark Goetz <breakawayblue@gmail.com>
2022-01-29 17:09:51 -0600

    Add export button

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,9 +11,14 @@ const Footer: React.FC = () => {
 
     return (
         <div className="c-footer">
-            <span className="u-txt--24 u-txt--heading u-txt--weight-black u-txt--color-black">
-                Total cards: {allCards.length}/{cardCount}
-            </span>
+            <div className="o-h-list o-h-list--x2 o-h-list--center">
+                <span className="u-txt--24 u-txt--heading u-txt--weight-black u-txt--color-black">
+                    Total cards: {allCards.length}/{cardCount}
+                </span>
+                <button type="button" disabled={allCards.length !== cardCount} className="c-btn">
+                    Export to Arena
+                </button>
+            </div>
         </div>
     );
 };
