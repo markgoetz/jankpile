88093109f622d4f72e5fb9a46634b434c77c53b9
Mark Goetz <breakawayblue@gmail.com>
2022-01-30 21:24:47 -0600

    Add basics function

--- src/functions/basics.ts
+++ src/functions/basics.ts
@@ -1,16 +1,17 @@
 import { Handler } from '@netlify/functions';
 import axios, { AxiosResponse } from 'axios';
-import CardResponse from '../../definitions/dto/CardResponse';
-import { createResponse, getEndpoint, parseColors, parseFormat } from '../../lib/function-utils/helpers';
-import { fullCardToLand } from '../../lib/translation/cardTranslations';
+import CardResponse from '../definitions/dto/CardResponse';
+import { createResponse, getEndpoint } from '../lib/function-utils/helpers';
+import { fullCardToLand } from '../lib/translation/cardTranslations';
 
 const handler: Handler = async(event, context) => {
     const params = event.queryStringParameters ?? {};
 
-    const colorString = parseColors(params);
-    const format = parseFormat(params);
+    const color = params.color;
+    const isSnow = params.isSnow != null;
 
-    const cardQuery = `id<=${colorString}+f:${format}+t:land+-t:basic+game:arena`;
+    const snowQuery = isSnow ? 't:snow' : '-t:snow';
+    const cardQuery = `id=${color}+f:brawl+t:land+${snowQuery}+t:basic+game:arena&unique=art`;
 
     try {
         const response = await axios.get(getEndpoint(cardQuery)) as AxiosResponse<CardResponse>;
