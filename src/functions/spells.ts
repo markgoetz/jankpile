688f4483be21c8c9946b4074865dfcdf26ce26cb
Mark Goetz <breakawayblue@gmail.com>
2022-01-31 21:14:31 -0600

    Add pagination to spell endpoint

--- src/functions/spells.ts
+++ src/functions/spells.ts
@@ -4,6 +4,10 @@ import CardResponse from '../definitions/dto/CardResponse';
 import { createResponse, getEndpoint, parseColors, parseFormat } from '../lib/function-utils/helpers';
 import { fullCardToSpell } from '../lib/translation/cardTranslations';
 
+const API_RESULTS = 175;
+const PAGE_RESULTS = 25;
+const CLIENT_PAGES_PER_SERVER_PAGE = API_RESULTS / PAGE_RESULTS;
+
 const handler: Handler = async(event, context) => {
     const params = event.queryStringParameters ?? {};
 
@@ -11,12 +15,17 @@ const handler: Handler = async(event, context) => {
     const format = parseFormat(params);
     const manaValues = params.mv?.split(',') ?? [];
     const query = params.q ?? '';
+    const requestedPage = parseInt(params.p ?? '0', 10);
+
+    const serverPage = Math.floor(requestedPage / CLIENT_PAGES_PER_SERVER_PAGE) + 1;
+    const clientSplice = requestedPage % CLIENT_PAGES_PER_SERVER_PAGE;
 
     const queryPieces = [
         `id<=${colorString}`,
         `f:${format}`,
         'game:arena',
-        '-t:land'
+        '-t:land',
+        `page:${serverPage}`,
     ];
 
     if (manaValues.length > 0) {
@@ -28,12 +37,12 @@ const handler: Handler = async(event, context) => {
     }
 
     const cardQuery = queryPieces.join('+');
-    console.log(cardQuery);
 
     try {
         const response = await axios.get(getEndpoint(cardQuery)) as AxiosResponse<CardResponse>;
         const parsedResponse = response.data.data.map(fullCardToSpell);
-        return createResponse(200, parsedResponse);
+        const splicedResponse = parsedResponse.splice(clientSplice * PAGE_RESULTS, PAGE_RESULTS);
+        return createResponse(200, splicedResponse);
     } catch (e) {
         return createResponse(500, e);
     }
