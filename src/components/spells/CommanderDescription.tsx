0338b326ef803f9dc035b184233f48a9b9540626
Mark Goetz <breakawayblue@gmail.com>
2022-02-21 19:14:06 -0500

    Add content container around commander description

--- src/components/spells/CommanderDescription.tsx
+++ src/components/spells/CommanderDescription.tsx
@@ -6,13 +6,15 @@ type Props = {
 };
 
 const CommanderDescription: React.FC<Props> = ({ commander }) => {
-    const descriptionPieces = commander?.description?.split('\n') ?? [];
+    const descriptionLines = commander?.description?.split('\n') ?? [];
 
     return (
         <div className="o-h-list o-h-list--x2">
             <div><img src={commander?.fullImageUri} alt={commander?.name} /></div>
-            <div className="o-v-list o-v-list--x2">
-                {descriptionPieces.map(piece => <span key={piece} className="u-txt--lh-1.4">{piece}</span>)}
+            <div className="o-content-container">
+                <div className="o-v-list o-v-list--x2">
+                    {descriptionLines.map(piece => <span key={piece} className="u-txt--lh-1.4">{piece}</span>)}
+                </div>
             </div>
         </div>
     );
