152058c5b132f59eaa88bcc3c79bdb6a9353d098
Mark Goetz <breakawayblue@gmail.com>
2022-03-13 23:28:29 -0500

    Add tooltip layout

--- src/components/common/CardTooltip.tsx
+++ src/components/common/CardTooltip.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Card from '../../definitions/Card';
+import Button from './Button';
+import X_SVG from '../../assets/images/x-brown.svg';
 
 type Props = {
     option: Card,
@@ -11,9 +13,21 @@ const CardTooltip: React.FC<Props> = ({ option, onClose }) => {
 
     return (
         <span className="c-card-tooltip">
-            <img src={option.fullImageUri} alt={option.name} width={146} height={204} />
-            <span className="c-card-tooltip__description">
-                {descriptionPieces.map(piece => <p key={piece} className="u-txt--lh-1.4">{piece}</p>)}
+            <span className="c-card-tooltip__layout">
+                <span className="c-card-tooltip__image">
+                    <img src={option.fullImageUri} alt={option.name} width={146} height={204} />
+                </span>
+                <span className="c-card-tooltip__description">
+                    {descriptionPieces.map(piece => <p key={piece} className="u-txt--lh-1.4">{piece}</p>)}
+                </span>
+                <span className="c-card-tooltip__close">
+                    <button type="button" onClick={onClose}>
+                        <img src={X_SVG} alt="Close" />
+                    </button>
+                </span>
+                <span className="c-card-tooltip__button">
+                    <Button>Add card</Button>
+                </span>
             </span>
         </span>
     )
