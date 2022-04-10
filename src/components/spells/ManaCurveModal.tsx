7b9b48a7fae7177802fc001c9901787551eb1fc7
Mark Goetz <breakawayblue@gmail.com>
2022-04-10 16:37:42 -0500

    Separate creature and noncreature spells

--- src/components/spells/ManaCurveModal.tsx
+++ src/components/spells/ManaCurveModal.tsx
@@ -14,14 +14,25 @@ type Props = {
 const MAX_MANA_VALUE = 7;
 
 const ManaCurveModal: React.FC<Props> = ({ isOpen, onClose, spells }) => {
+    const creatures = spells.filter(spell => spell.frontFace.type.includes('Creature'));
+    const noncreatures = spells.filter(spell => !spell.frontFace.type.includes('Creature'));
+
     const manaValues = getManaValues(spells, MAX_MANA_VALUE);
-    const creatureCount = spells.filter(spell => spell.frontFace.type.includes('Creature')).length;
-    const nonCreatureCount = spells.filter(spell => !spell.frontFace.type.includes('Creature')).length;
+    const creatureManaValues = getManaValues(creatures, MAX_MANA_VALUE);
+    const noncreatureManaValues = getManaValues(noncreatures, MAX_MANA_VALUE);
+
     const maxManaValue = Math.max(...Object.values(manaValues));
-    const lengths = Object.keys(manaValues).reduce(
+    const creaturesLengths = Object.keys(creatureManaValues).reduce(
         (prevLengths, manaValue) => ({
             ...prevLengths,
-            [manaValue]: manaValues[Number(manaValue)] / maxManaValue,
+            [manaValue]: creatureManaValues[Number(manaValue)] / maxManaValue,
+        }),
+        {} as Record<number, number>,
+    );
+    const noncreaturesLengths = Object.keys(noncreatureManaValues).reduce(
+        (prevLengths, manaValue) => ({
+            ...prevLengths,
+            [manaValue]: noncreatureManaValues[Number(manaValue)] / maxManaValue,
         }),
         {} as Record<number, number>,
     );
@@ -35,11 +46,11 @@ const ManaCurveModal: React.FC<Props> = ({ isOpen, onClose, spells }) => {
                     <tbody>
                         <tr>
                             <th scope="row" className="c-table__cell u-txt--align-right">Creatures</th>
-                            <td className="c-table__cell u-txt--align-left">{creatureCount}</td>
+                            <td className="c-table__cell u-txt--align-left">{creatures.length}</td>
                         </tr>
                         <tr>
                             <th scope="row" className="c-table__cell u-txt--align-right">Noncreatures</th>
-                            <td className="c-table__cell u-txt--align-left">{nonCreatureCount}</td>
+                            <td className="c-table__cell u-txt--align-left">{noncreatures.length}</td>
                         </tr>
                     </tbody>
                 </table>
@@ -56,7 +67,8 @@ const ManaCurveModal: React.FC<Props> = ({ isOpen, onClose, spells }) => {
                                         <span className="o-h-list">
                                             <span>{manaValues[Number(value)] ?? 0}</span>
                                             <span className="c-bar">
-                                                <span className="c-bar__inner" style={{width: `${lengths[Number(value)] * 100}%`}} />
+                                                <span className="c-bar__inner" style={{width: `${creaturesLengths[Number(value)] * 100}%`}} />
+                                                <span className="c-bar__inner c-bar__inner--highlight" style={{width: `${noncreaturesLengths[Number(value)] * 100}%`}} />
                                             </span>
                                         </span>
                                     </td>
