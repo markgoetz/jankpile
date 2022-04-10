8eab1d140dc50c244622b0d219c7bb5abc709176
Mark Goetz <breakawayblue@gmail.com>
2022-04-10 16:28:00 -0500

    Add chart to mana curve modal

--- src/components/spells/ManaCurveModal.tsx
+++ src/components/spells/ManaCurveModal.tsx
@@ -17,6 +17,14 @@ const ManaCurveModal: React.FC<Props> = ({ isOpen, onClose, spells }) => {
     const manaValues = getManaValues(spells, MAX_MANA_VALUE);
     const creatureCount = spells.filter(spell => spell.frontFace.type.includes('Creature')).length;
     const nonCreatureCount = spells.filter(spell => !spell.frontFace.type.includes('Creature')).length;
+    const maxManaValue = Math.max(...Object.values(manaValues));
+    const lengths = Object.keys(manaValues).reduce(
+        (prevLengths, manaValue) => ({
+            ...prevLengths,
+            [manaValue]: manaValues[Number(manaValue)] / maxManaValue,
+        }),
+        {} as Record<number, number>,
+    );
 
     return (
         <Modal size="small" isOpen={isOpen} onClose={onClose}>
@@ -38,14 +46,19 @@ const ManaCurveModal: React.FC<Props> = ({ isOpen, onClose, spells }) => {
             </div>
             <div className="u-vr--x2" />
             <div className="u-center">
-                <table className="c-table u-txt--color-primary u-txt--20">
+                <table className="c-table c-table--full-width u-txt--color-primary u-txt--20">
                     <tbody>
-                        {Object.keys(manaValues).map(
+                        {[1, 2, 3, 4, 5, 6, 7].map(
                             value => (
                                 <tr key={value}>
-                                    <td className="c-table__cell u-txt--align-right">Cost {value}</td>
-                                    <td className="c-table__cell u-txt--align-left">
-                                        {manaValues[Number(value)]}
+                                    <td className="c-table__cell u-txt--align-right">Cost&nbsp;{value}</td>
+                                    <td className="c-table__cell c-table__cell--full-width u-txt--align-left">
+                                        <span className="o-h-list">
+                                            <span>{manaValues[Number(value)] ?? 0}</span>
+                                            <span className="c-bar">
+                                                <span className="c-bar__inner" style={{width: `${lengths[Number(value)] * 100}%`}} />
+                                            </span>
+                                        </span>
                                     </td>
                                 </tr>
                             )
