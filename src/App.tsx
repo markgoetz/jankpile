dc5220e35e47980363c650033ee2c38e3286c32a
Mark Goetz <breakawayblue@gmail.com>
2022-06-05 11:19:28 -0400

    Create Main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import Header from './components/Header';
-import Footer from './components/Footer';
-import IdentityPanel from './components/IdentityPanel';
-import CommanderPanel from './components/commander/CommanderPanel';
-import SpellPanel from './components/spells/SpellPanel';
-import LandPanel from './components/lands/LandPanel';
 import store from './redux-modules/store';
-import ExportModal from './components/ExportModal';
+import Main from './components/Main';
 
 const App: React.FC = () => {
-    const [isExportModalOpen, setIsExportModalOpen] = useState(false);
-
     return (
         <Provider store={store}>
             <div className="App">
                 <header><Header /></header>
-                <main>
-                    <section><IdentityPanel /></section>
-                    <section><CommanderPanel /></section>
-                    <section><SpellPanel /></section>
-                    <section><LandPanel /></section>
-                    <section><Footer onExportClick={() => setIsExportModalOpen(true)} /></section>
-                </main>
+                <main><Main /></main>
             </div>
-            <ExportModal isOpen={isExportModalOpen} onClose={() => { setIsExportModalOpen(false); }} />
         </Provider>
     );
 }
---
+++ src/components/Main.tsx
@@ -0,0 +1,29 @@
+import React, { useRef, useState } from 'react';
+import Footer from './Footer';
+import IdentityPanel from './IdentityPanel';
+import CommanderPanel from './commander/CommanderPanel';
+import SpellPanel from './spells/SpellPanel';
+import LandPanel from './lands/LandPanel';
+import ExportModal from './ExportModal';
+
+const Main: React.FC = () => {
+    const [isExportModalOpen, setIsExportModalOpen] = useState(false);
+    const identityRef = useRef<HTMLElement>(null);
+    const commanderRef = useRef<HTMLElement>(null);
+    const spellRef = useRef<HTMLElement>(null);
+    const landRef = useRef<HTMLElement>(null);
+
+    return (
+        <React.Fragment>
+            <section ref={identityRef}><IdentityPanel /></section>
+            <section ref={commanderRef}><CommanderPanel /></section>
+            <section ref={spellRef}><SpellPanel /></section>
+            <section ref={landRef}><LandPanel /></section>
+            <section><Footer onExportClick={() => setIsExportModalOpen(true)} /></section>
+            <ExportModal isOpen={isExportModalOpen} onClose={() => { setIsExportModalOpen(false); }} />
+
+        </React.Fragment>
+    );
+};
+
+export default Main;
