e7603b961da7b0f1088533203e3270bf7b15b4ea
Mark Goetz <breakawayblue@gmail.com>
2022-06-11 10:18:43 -0500

    Add dropdown component

---
+++ src/components/common/Dropdown.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export type Option = {
+    label: string,
+    value: string,
+};
+
+type Props = {
+    options: Option[],
+    value: string,
+    onSelect: (value: string) => void,
+};
+
+const Dropdown: React.FC<Props> = ({ options, value, onSelect }) => {
+    const onChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        onSelect(e.target.value);
+    };
+
+    return (
+        <select value={value} onChange={onChange} className="c-dropdown">
+            {options.map(option => (
+                <option value={option.value}>{option.label}</option>
+            ))}
+        </select>
+    );
+};
+
+export default Dropdown;
