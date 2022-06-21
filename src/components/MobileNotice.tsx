87473508268095905166fd107ebd3344584be80c
Mark Goetz <breakawayblue@gmail.com>
2022-06-20 22:36:11 -0500

    Styling adjustments

--- src/components/MobileNotice.tsx
+++ src/components/MobileNotice.tsx
@@ -6,7 +6,7 @@ const MobileNotice: React.FC = () => {
         <div className="u-visible--sm o-notice">
             <div className="o-notice__inner">
                 <Heading size="large" tag="h1">Oh dear.</Heading>
-                <div className="u-vr" />
+                <div className="u-vr--x2" />
                 <span className="u-txt u-txt--color-black u-txt--24">
                     Jankpile is meant to be viewed on devices at least 1024 pixels wide.  Please expand your browser width if you can!
                 </span>
